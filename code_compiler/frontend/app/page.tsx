94f8ccc72b7e974c90da628a9f4f4fe46ea7bea4
MikhailVoznyuk <159428971+MikhailVoznyuk@users.noreply.github.com>
2025-09-18 17:41:42 +0300

    initial commit

---
+++ code_compiler/frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import CodeEditor from "@/components/CodeEditor/CodeEditor";
+
+export default function Home() {
+  return (
+    <main className='flex justify-center w-full p-20'>
+        <CodeEditor />
+    </main>
+  );
+}
