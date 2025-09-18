6d8843d86cbb27cd3eed3eb530cea8cb1d31dc8e
MikhailVoznyuk <159428971+MikhailVoznyuk@users.noreply.github.com>
2025-09-18 17:46:00 +0300

    initial commit

--- code_compiler/frontend/components/CodeEditor/contexts.ts
+++
@@ -1,6 +0,0 @@
-import {createContext, Dispatch} from "react";
-import type {Action, State} from "@/components/CodeEditor/CodeEditor.types";
-
-export const StateContext = createContext<State | null>(null)
-
-export const DispatchContext = createContext<Dispatch<Action> | null>(null)
